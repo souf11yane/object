eb7fd881264fab9c5f0948a678bc7bfb067bb3db
Soufiane Oukattou <37966094+souf11yane@users.noreply.github.com>
2023-07-14 19:08:51 +0100

    update github action

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     coverage: {
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "clover"],
     },
   },
 });
